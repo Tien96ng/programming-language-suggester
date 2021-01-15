d2fb3f31a27b351908e0297107d0cd1bfe5b39be
tien96ng <tien96ng@ghmail.com>
2021-01-15 14:02:07 -0800

    Create function to render next question using branching to determine which type of question/input type it will be.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let questionCount = 0;
+let userAnswers = [];
 let questionsBank = [
-  {
+  { // Condition Done
     question: "(10 x 7 / 5) + 3)",
     answer: {
       type: "input number",
@@ -8,15 +9,7 @@ let questionsBank = [
       answers: 17
     }
   },
-  {
-    question: "Select the applicable answer to you:",
-    answer: {
-      type: "radio",
-      id: "time",
-      answers: ["Early Bird", "Late Nighter", "Love Sleeping In"]
-    }
-  },
-  {
+  { // Condition Done
     question: "Enter in your first name:",
     answer: {
       type: "input text",
@@ -24,7 +17,7 @@ let questionsBank = [
       answers: ""
     }
   },
-  {
+  { // Condition Done
     question: "Select your birthdate:",
     answer: {
       type: "date",
@@ -39,33 +32,65 @@ let questionsBank = [
       id: "color",
       answers: ["Blue", "Orange", "Purple", "Yellow", "Red"] // Python, Java, C#, JS, Ruby
     }
+  },
+  { // Condition Done
+    question: "Select the applicable answer to you:",
+    answer: {
+      type: "radio",
+      id: "time",
+      answers: ["Early Bird", "Late Nighter", "Love Sleeping In"]
+    }
   }
 ];
 
 
 $(document).ready(function() {
+
   $("#start-btn").click(function() {
     $("h4").hide();
     $("#start-btn").hide();
     $("form#quiz").show();
 
-    /*
-    for(let i = 0; i < questionsBank.length; i++) {
-      console.log(questionsBank[i])
-      $("#placeholder-label").text(questionsBank[i].question);
-      $("#placeholder-label").attr("for", questionsBank[i].answer.id);
-      if(questionsBank[i].answer.type.includes("input")) {
-        $("#placeHolder").attr("type", questionsBank[i].answer.type.split(" ")[1]);
-        $("#placeHolder").attr("name", questionsBank[i].answer.type.split(" ")[1]);
-      }
-    }
+    let currentQuestionSet = questionsBank[questionCount];
+    console.log(currentQuestionSet)
 
-    */
-    while(questionCount <= questionsBank.length) {
-      let currentQuestionSet = questionsBank[questionCount];
-      console.log(currentQuestionSet)
-      console.log(`${questionCount} === ${questionsBank.length}`)
-      // questionCount++; Only use when they click NEXT button.
+    function renderNextQuestion(question, id, type, answers) {
+      if(type === "radio") {
+        $("placeholder-label").remove();
+        $("placeHolder").remove();
+        let radioNum = 1;
+
+        for(let i = 0; i < answer.length; i++) {
+          let createLabel = `<label for="${id + radioNum}"> ${question} </label>`;
+          let createInput = `<input type="radio" id="${id + radioNum}" name="radio">`;
+          $("#quiz").append(createLabel);
+          $("#quiz").append(createLabel);
+        }
+
+      } else {
+        $("#placeholder-label").text(`${questionCount + 1}.${question}`);
+        $("#placeholder-label").attr("for", id);
+
+        if(type.includes("input")) {
+          $("#placeHolder").attr("input", type.split(" ")[1]);
+          $("#placeHolder").attr("input", type.split(" ")[1]);
+        } else {
+          $("placeHolder").remove();
+          $("#quiz").append(`<select name="${id} id="${id}> </select>"`)
+          
+          for(let i = 0; i < answer.length; i++) {
+            let createOption = `<option value="${answer[i]}">${answer[i]}</option>`;
+            $("#color").append(createOption);
+          }
+        }
+      }
+
+      
+
     }
+    // console.log(`${questionCount} === ${questionsBank.length}`)
+    //$("#placeholder-label").text(currentQuestionSet.question);
+    //$("#placeholder-label").attr("for", currentQuestionSet.answer.id);
+    // questionCount++; Only use when they click NEXT button.
   });
 });
