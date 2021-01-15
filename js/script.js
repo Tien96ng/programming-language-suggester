728dcab780280711c8d7b3ce5bf8b4130ae285e3
tien96ng <tien96ng@ghmail.com>
2021-01-15 12:27:05 -0800

    Add placeholder form with label, input, next button, and finish/submit quiz button.

--- js/script.js
+++ js/script.js
@@ -5,44 +5,38 @@ let questionsBank = {
       type: "dropdown",
       answers: ["Blue", "Orange", "Purple", "Yellow", "Red"] // Python, Java, C#, JS, Ruby
     }
-  }
+  },
+  questionTwo: {
+    question: "(10 x 7 / 5) + 3)",
+    answer: {
+      type: "input number",
+      answers: 17
+    }
+  },
+  questionThree: {
+    question: "Select the applicable answer to you:",
+    answer: {
+      type: "radio",
+      answers: ["Early Bird", "Late Nighter", "Love Sleeping In"]
+    }
+  },
+  questionFour: {
+    question: "Enter in the first letter of your name:",
+    answer: {
+      type: "input text",
+      answers: ""
+    }
+  },
+  questionFive: {
+    question: "Select your birthdate:",
+    answer: {
+      type: "date",
+      answers: 0
+    }
+  },
 }
 
 
-
-
-
-
-
-
-
-
-
 $(document).ready(function() {
-  $("form#quiz").submit(function(event) {
-    event.preventDefault();
-
-    const color = $("#fav-color").val();
-    const number = $("#math-problem").val();
-    const pbj = $("#pbj option:selected").val();
-    const programmer = $("#type-programmer option:selected").val();
-    const walk = $("#walk").is(":checked");
-    const run = $("#run:checked").is(":checked");
-    const sleep = $("#sleep:checked").is(":checked");
-
-
-    console.log({
-      color, number, pbj, programmer, walk, run, sleep
-    });
-
-    if(sleep) {
-      console.log("You're a Python Programmer.")
-    } else {
-      if(walk) {
-        console.log("You're a JavaScript Programmer")
-      } else {
-        console.log("You're a Java Programmer")
-      }
-    }
-  })
+  
 });
